e82b07e48c82dfd83530bc7d0782f5b0b750c9c8
mammont <mamontov_misha@mail.ru>
2021-04-22 13:05:31 +0300

    ДЗ №4 готово.

---
+++ src/Chat/Controller.java
@@ -0,0 +1,35 @@
+package Chat;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+
+public class Controller {
+
+    @FXML
+    TextField chatField;
+
+    @FXML
+    TextArea chatArea;
+
+    private void sendMessage(){
+        if(!chatField.getText().equals("")) {
+            chatArea.appendText(chatField.getText() + "\n");
+            chatField.clear();
+        }
+    }
+
+    public void clickSendBtn(ActionEvent actionEvent) {
+        sendMessage();
+    }
+
+    public void clickEnter(ActionEvent actionEvent) {
+        chatField.setOnKeyPressed(event -> {
+            if(event.getCode().equals(KeyCode.ENTER)){
+                sendMessage();
+            }
+        });
+    }
+}
